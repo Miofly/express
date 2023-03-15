c3b718adc7866611ef08daf4b1ce5b8209537324
wfly <953212389@qq.com>
2023-03-15 13:38:12 +0800

    1

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -4,7 +4,7 @@ import { resultSuccess } from '../../utils';
 export const userRouter = Router({})
 
 userRouter.get('/', async(req: Request, res: Response) => {
-  res.send('http://wflynn.cn')
+  res.redirect('http://wflynn.cn')
 })
 
 userRouter.get('/getUserInfo', async ( req: Request, res: Response) => {
