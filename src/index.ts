c025ff7938adb3d53053ff9511cc0ef5d2c2a48a
wfly <953212389@qq.com>
2023-03-15 13:48:18 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,15 @@ const app = express()
 const port = process.env.PORT || 5000
 
 const parserMiddleware = express.json()
+
+app.all('*', (_: any, res: any, next: () => void) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1');
+  next();
+})
+
 app.use(parserMiddleware)
 
 app.use('/', userRouter)
