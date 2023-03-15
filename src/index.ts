77baa9a297fd3ede2b6f744228f51f83d271b935
wfly <953212389@qq.com>
2023-03-15 13:27:28 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import { userRouter } from './routers';
+import { resultSuccess } from './utils';
 
 const app = express()
 const port = process.env.PORT || 5000
 
 const parserMiddleware = express.json()
 app.use(parserMiddleware)
+
+app.use('/', (req, res) => {
+  res.redirect('https://wflynn.cn/home')
+})
 
 app.use('/', userRouter)
 
