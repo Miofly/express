a5662252378e90fdfe30a084843411172010f887
wfly <953212389@qq.com>
2023-03-15 15:10:10 +0800

    feat: 1

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import express from 'express'
+import express from 'express';
 import { userRouter } from './routers';
-import { resultSuccess } from './utils';
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-const parserMiddleware = express.json()
+const parserMiddleware = express.json();
 
 app.all('*', (_: any, res: any, next: () => void) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('X-Powered-By', ' 3.2.1');
   next();
-})
+});
 
-app.use(parserMiddleware)
+app.use(parserMiddleware);
 
-app.use('/', userRouter)
+app.use('/', userRouter);
 
 const startApp = async () => {
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-}
+    console.log(`Example app listening on port ${port}`);
+    console.log(`Example http://localhost:${port}/getUserInfo`);
+  });
+};
 
-startApp()
+startApp();
