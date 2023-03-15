cbf88d3d03c3df54daef00808809603810d0cac2
wfly <953212389@qq.com>
2023-03-15 13:33:18 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,7 @@ const port = process.env.PORT || 5000
 const parserMiddleware = express.json()
 app.use(parserMiddleware)
 
-app.use('/', (req, res) => {
-  res.redirect('https://wflynn.cn/home')
-})
-
-app.use('/user', userRouter)
+app.use('/', userRouter)
 
 const startApp = async () => {
   app.listen(port, () => {
--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -3,6 +3,10 @@ import { resultSuccess } from '../../utils';
 
 export const userRouter = Router({})
 
+userRouter.get('/', async(req: Request, res: Response) => {
+  res.redirect('http://wflynn.cn')
+})
+
 userRouter.get('/getUserInfo', async ( req: Request, res: Response) => {
   res.json(resultSuccess([
     {
