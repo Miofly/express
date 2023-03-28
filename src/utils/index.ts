9510e14827aa574d5396c67f30bb33ea67b3b023
wfly <953212389@qq.com>
2023-03-28 12:07:39 +0800

    feat: 1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,32 @@
+export function pagination<T = any> (pageNo: number, pageSize: number, array: T[]): T[] {
+  const offset = (pageNo - 1) * Number(pageSize);
+  return offset + Number(pageSize) >= array.length
+    ? array.slice(offset, array.length)
+    : array.slice(offset, offset + Number(pageSize));
+}
+
+export function resultPageSuccess<T = any> (
+  page: number,
+  pageSize: number,
+  list: T[],
+  { msg = 'ok' } = {}
+) {
+  const pageData = pagination(page, pageSize, list);
+  
+  return {
+    ...resultSuccess({
+      items: pageData,
+      total: list.length
+    }),
+    msg
+  };
+}
+
 export function resultSuccess<T = any> (result: T, { message = 'ok' } = {}) {
-	return {
-		code: 200,
-		data: result,
-		msg: message,
-		type: 'success'
-	};
+  return {
+    code: 200,
+    data: result,
+    msg: message,
+    type: 'success'
+  };
 }
