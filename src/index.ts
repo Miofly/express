ce5cf86a7e38e30f961cb8f6228cfc8b85466a94
wfly <953212389@qq.com>
2023-04-03 17:26:46 +0800

    feat: 1

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const parserMiddleware = express.json();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+  res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Request-Method,application/json;charset=UTF-8');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE');
   res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
   res.header('X-Powered-By', ' 3.2.1');
