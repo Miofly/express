70b0787a431f12a05327ea824c3aa262155bf8d9
wfly <953212389@qq.com>
2023-06-08 21:59:44 +0800

    feat: update project

--- src/routers/table/index.ts
+++ src/routers/table/index.ts
@@ -4,18 +4,23 @@ import { faker } from '@faker-js/faker';
 
 export const tableRouter = Router({});
 
+
+
 const accountList = (() => {
   const result: any[] = [];
   for (let index = 0; index < 93; index++) {
     result.push({
       id: `${index}`,
+      name: faker.internet.userName(),
+
       account: faker.datatype.uuid(),
       avatar: faker.image.image(),
       password: faker.internet.password(),
       birthdate: faker.date.birthdate(),
       registeredAt: faker.date.past(),
       email: faker.internet.email(),
-      nickname: faker.internet.userName()
+      animal: faker.helpers.arrayElements(['cat', 'dog', 'mouse']),
+      status: faker.helpers.arrayElements([1, 0])[0]
     });
   }
   return result;
